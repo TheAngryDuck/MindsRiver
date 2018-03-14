3293c5d4285eef6703ac6469a546617d42f04542
179466 <maobli@ttu.ee>
2018-03-14 23:46:00 +0200

    more blocks

--- denied.js
+++ denied.js
@@ -48,6 +48,26 @@ if (document.layers) {
     document.onmouseup=clickNS;
     document.oncontextmenu=clickIE;
 }
+
 document.oncontextmenu=new Function("return false")
+document.onkeydown = function(e) {
+    if(event.keyCode === 123) {
+        alert("NO!");
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)){
+        alert("NO!");
+        return false;
+
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)){
+        alert("NO!");
+        return false;
+    }
+    if(e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)){
+        alert("NO!");
+        return false;
+    }
+}
 
 
