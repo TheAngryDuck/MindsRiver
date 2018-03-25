916008ed66c0e7affa2f5480b3167362a8bcc6de
179466 <maobli@ttu.ee>
2018-03-25 15:24:44 +0300

    added the second puzzle to lvl2

---
+++ feedback.js
@@ -0,0 +1,17 @@
+function travelPlan() {
+    var guess = document.getElementById("feedbackPass").value;
+
+    console.log("guess: " + guess);
+    if (guess.toLowerCase() === "superman"){
+        alert("Now, superman is indeed very super, but what if he was less super and more of a Sinner. What would he be called then?" +
+            "\n\n What was the last place/person to where he went to hide?");
+} else if(guess.toLowerCase() === "devil"){
+        alert("Now, we know that the devil is a known deceiver and hides behind many faces. Maybe the great fighters of the foo know anything about it." +
+            "\n\n What will the fighter never do?");
+    }else if (guess.toLowerCase() === "surrender"){
+        alert("Now, in the north there are a group of warriors who also will never surrender. They go by a silly name like Sabaton or something. Could they hold your final answer?" +
+            "\n\n Where does the one way trip lead?");
+    }else if (guess.toLowerCase() === "auschwitz"){
+        alert("Your final answer is: desc");
+    }
+}
