92a9a50dc6b8651dcd66b7b3c6e470f84ff38370
TheAngryDuck <martin.oblikas@gmail.com>
2018-04-14 19:48:56 +0300

    map puzzle.

---
+++ mapMessage.js
@@ -0,0 +1,17 @@
+function message() {
+    alert("To get to the prize you have to follow these lines.\n " +
+        "Take yourself to the end of the line.\n Turn your sight to the road on the right.\n " +
+        "To the end of the road you now must go.\n " +
+        "Your face is met with the road on your left.\n" +
+        "Take a break at the second cross, for goodness sake.\n" +
+        "Now you must go down the right road.\n" +
+        "To your left a road leads, and go to the end, if you please.\n" +
+        "To the base of that building you now are heading.\n" +
+        "On the next road, you must go ahead and then right.\n" +
+        "Travel that road, to the second cross or so.\n" +
+        "Now go right, then forward and left.\n" +
+        "After treading that road, another right, another left.\n" +
+        "After a right another cross you must face.\n" +
+        "By taking a right a familiar place you will see.\n" +
+        "Taking right twice leads to a building most grand. In the middle of that land your answer will stand.");
+}
